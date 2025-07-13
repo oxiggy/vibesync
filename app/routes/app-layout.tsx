7d38f1da4d98c5402c594dea44a462eb769662f2
Ada Void <adavoid@proton.me>
2025-07-13 16:55:59 +0400

    feat: implement theme support

--- app/routes/app-layout.tsx
+++ app/routes/app-layout.tsx
@@ -1,5 +1,14 @@
 import { Outlet } from 'react-router'
+import AppBar from '@/scopes/app/app-bar'
+import ThemeProvider from '@/scopes/app/theme-provider'
 
 export default function AppLayout() {
-	return <Outlet />
+	return (
+		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+			<div className="min-h-dvh h-full grid grid-rows-[auto_1fr] overflow-hidden">
+				<AppBar />
+				<Outlet />
+			</div>
+		</ThemeProvider>
+	)
 }
