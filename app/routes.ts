1e0c7441ba2d9b976a3c5164534e26e94d142afa
Mickh <tehfreak@tehfreak.com>
2025-08-29 22:45:03 +0300

    Format code

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,13 @@ import { type RouteConfig, index, route, layout } from '@react-router/dev/routes
 
 export default [
 	index('routes/home.tsx'),
-	layout('routes/app-layout.tsx', [route('dashboard', 'routes/dashboard.tsx'), route('games', 'routes/games.tsx'), route('games/:gameId', 'routes/game.tsx'), route('games/create', 'routes/game-create.tsx'),  route('forms', 'routes/forms.tsx'), route('forms/:formId', 'routes/form.tsx')]),
 	route('api/game/create', 'routes/api/game/create.ts'),
+	layout('routes/app-layout.tsx', [
+		route('dashboard', 'routes/dashboard.tsx'),
+		route('games', 'routes/games.tsx'),
+		route('games/:gameId', 'routes/game.tsx'),
+		route('games/create', 'routes/game-create.tsx'),
+		route('forms', 'routes/forms.tsx'),
+		route('forms/:formId', 'routes/form.tsx'),
+	]),
 ] satisfies RouteConfig
